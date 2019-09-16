8758f9f64eed2bdae97098b7bbdbdd9f30593039
hy165 <hy165@duke.edu>
2019-09-16 16:09:38 -0400

    assignment 037

---
+++ 037_array_subseq/maxSeq.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t maxSeq(int * array, size_t n) {
+  size_t result = 0;
+  size_t i = 1, j = 0;
+  if (n == 0) {
+    return 0;
+  }
+  for (i = 1; i < n; i++) {
+    if (array[i] <= array[i - 1]) {
+      if ((i - j) > result) {
+        result = i - j;
+      }
+      j = i;
+    }
+  }
+
+  return result;
+}
