b3efbcf13430c3e84e8a9f8b725dbbf4cb900ff3
hy165 <hy165@duke.edu>
2019-09-17 14:31:05 -0400

    fix array

--- 036_tests_subseq/test-subseq.c
+++ 036_tests_subseq/test-subseq.c
@@ -10,9 +10,10 @@ void check_seq(int array[], size_t n, size_t expected_ans) {
     exit(EXIT_FAILURE);
   }
 }
-int array0[] = {0}, array1[] = {1}, array2[] = {1, 1}, array3[] = {1, 2, 3, 4, 5, 5},
+int array1[] = {1}, array2[] = {1, 1}, array3[] = {1, 2, 3, 4, 5, 5},
     array4[] = {1, 2, 3, 4, 5, 7, 2, 4, 6, 9, 10}, array5[] = {-3, -2, -1, 1},
     array6[] = {-5, 4, -3, 2, -1, 0, 1, 2, 3, 4}, array7[] = {-5, -4, -3};
+int * array0;
 
 int main() {
   check_seq(array0, 0, 0);
