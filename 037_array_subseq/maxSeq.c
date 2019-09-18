dd166f1db7d2e01081f5741441f251080b02b5d8
hy165 <hy165@duke.edu>
2019-09-17 20:15:59 -0400

    assignment 037

--- 037_array_subseq/maxSeq.c
+++ 037_array_subseq/maxSeq.c
@@ -4,7 +4,7 @@
 size_t maxSeq(int * array, size_t n) {
   size_t result = 1, len = 1;
   size_t i = 1;
-  if (n <= 0) {
+  if (n == 0) {
     return 0;
   }
   for (i = 1; i < n; i++) {
@@ -14,8 +14,8 @@ size_t maxSeq(int * array, size_t n) {
     else {
       if (len > result) {
         result = len;
-        len = 1;
       }
+      len = 1;
     }
   }
   if (len > result) {
