417d198fad6ee05939fcf12c564d83b60eab6097
hy165 <hy165@duke.edu>
2019-09-18 16:13:41 -0400

    assignment 029 step2 and revised step1

---
+++ 029_num_seq/step2.c
@@ -0,0 +1,64 @@
+// This file is for Step 2.
+// You should do
+//  Step 2 (A): write seq2
+//  Step 2 (B): write main to test seq2
+//  Step 2 (C): write sumSeq2
+//  Step 2 (D): add test cases to main to test sumSeq2
+//
+// Be sure to #include any header files you need!
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int seq2(int x) {
+  if ((x - 2) % 4 == 0) {
+    return 2 * x;
+  }
+  else {
+    return x * x + 3 * x + 1;
+  }
+}
+
+int sumSeq2(int low, int high) {
+  if (low >= high) {
+    return 0;
+  }
+  else {
+    int sum = 0;
+    for (int i = low; i < high; i++) {
+      sum += seq2(i);
+    }
+    return sum;
+  }
+}
+
+void test_seq2(int x) {
+  int y = seq2(x);
+  printf("seq2(%d) = %d\n", x, y);
+}
+
+void test_sumSeq2(int low, int high) {
+  printf("sumSeq2(%d, %d) = %d\n", low, high, sumSeq2(low, high));
+  sumSeq2(low, high);
+}
+
+int main() {
+  test_seq2(0);
+  test_seq2(1);
+  test_seq2(2);
+  test_seq2(-4);
+  test_seq2(6);
+  test_seq2(10);
+  test_seq2(-1);
+  test_seq2(14);
+
+  test_sumSeq2(0, 0);
+  test_sumSeq2(0, 2);
+  test_sumSeq2(3, 6);
+  test_sumSeq2(9, 7);
+  test_sumSeq2(0, -2);
+  test_sumSeq2(-3, -1);
+
+  return 0;
+}
