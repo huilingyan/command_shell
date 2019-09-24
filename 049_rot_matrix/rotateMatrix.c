5c65324d4cc0a6e898743cfcfedec138a760bfae
hy165 <hy165@duke.edu>
2019-09-24 19:04:54 -0400

    ...

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -3,61 +3,88 @@
 #include <stdlib.h>
 #include <string.h>
 
-void error_check(char array[10][11], FILE * f) {
-  int i = 0;
+#define COLS 10
+#define ROWS 10
+
+void error(const char * msg) {
+  fprintf(stderr, "Invalid input: %s\n", msg);
+  exit(EXIT_FAILURE);
+}
+
+void read_line(char line[], FILE * f) {
+  int i;
   int ch;
-  while ((ch = fgetc(f)) != EOF) {
+
+  i = 0;
+  for (;;) {
+    ch = fgetc(f);
     switch (ch) {
-      case '\n': {
-        const char * a = strchr(array[i], '\n');
-        if ((a - array[i] != 10)) {
-          fprintf(stderr, "Incorrect number of columns!\n");
-          exit(EXIT_FAILURE);
+      case '\n':
+        if (i == COLS) {
+          line[COLS] = '\0';
+          return;
         }
-        i++;
-      }
-      case EOF: {
-        if (i != 10) {
-          fprintf(stderr, "Incorrect number of lines!\n");
-          exit(EXIT_FAILURE);
+        else {
+          error("line is too short");
         }
-      }
-      default: {
-        for (int j = 0; j < 10; j++) {
-          array[i][j] = (char)ch;
+        break;
+      case EOF:
+        if (i == COLS) {
+          error("unexpected EOF");
         }
-      }
+        else {
+          error("line is too short");
+        }
+        break;
+      default:
+        if (i < COLS) {
+          line[i++] = (char)ch;
+        }
+        else {
+          error("line is too long");
+        }
+        break;
     }
-  }
-}
-void rotate(char matrix[10][11]) {
-  for (int i = 0; i < 10; i++) {
-    for (int j = 0; j < 10; j++) {
-      fprintf(stdout, "%c", matrix[j][9 - i]);
-    }
-    printf("\n");
   }
 }
 
-int main(int argc, char ** argv) {
+void print_rotated(char M[ROWS][COLS]) {
+  int i, j;
+
+  for (i = 0; i < ROWS; i++) {
+    for (j = 0; j < COLS; j++) {
+      /* let M' be the rotated matrix,
+			 * then M'(i, j) = M(COLS-1-j, i) */
+      putchar(M[COLS - 1 - j][i]);
+    }
+    putchar('\n');
+  }
+}
+
+int main(int argc, char * argv[]) {
+  char matrix[ROWS][COLS];
+  FILE * f;
+  int i, ch;
+
   if (argc != 2) {
-    fprintf(stderr, "Usage: rotate matrix inputFileName\n");
-    return EXIT_FAILURE;
-  }  //check if the input file is a single line comment
+    fprintf(stderr, "Usage: rotateMatrix input\n");
+    exit(EXIT_FAILURE);
+  }
+  if ((f = fopen(argv[1], "r")) == 0) {
+    fprintf(stderr, "Could not open %s\n", argv[1]);
+    exit(EXIT_FAILURE);
+  }
+  for (i = 0; i < ROWS; i++) {
+    read_line(matrix[i], f);
+  }
+  /* test for EOF */
+  if ((ch = fgetc(f)) != EOF) {
+    fprintf(stderr, "Invalid input: file is too long (read %d instead of EOF)\n", ch);
+    exit(EXIT_FAILURE);
+  }
+  fclose(f);
 
-  FILE * f = fopen(argv[1], "r");
-  if (f == NULL) {
-    fprintf(stderr, "Cannot open the file!\n");
-    return EXIT_FAILURE;
-  }  //if filename does not exist
+  print_rotated(matrix);
 
-  char matrix[10][11];
-
-  error_check(matrix, f);
-  rotate(matrix);
-  if (fclose(f) != 0) {
-    fprintf(stderr, "Failed to close the input file\n");
-    return EXIT_FAILURE;
-  }  //check if file can be closed
-  return EXIT_SUCCESS;
+  return 0;
 }
