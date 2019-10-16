587e6abdc07c2515e0e67e7b9c81fa4261adedc7
hy165 <hy165@duke.edu>
2019-10-16 14:02:09 -0400

    070

---
+++ 070_point/point.cpp
@@ -0,0 +1,13 @@
+#include "point.h"
+
+#include <cmath>
+#include <cstdio>
+
+void Point::move(double dx, double dy) {
+  x += dx;
+  y += dy;
+}
+
+double Point::distanceFrom(const Point & p) {
+  return std::sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y - p.y));
+}
