6bf108bde599443e5d60b448c77a99a0026dfb87
hy165 <hy165@duke.edu>
2019-11-06 11:00:16 -0500

    096

--- 096_binsrch/search.cpp
+++ 096_binsrch/search.cpp
@@ -4,25 +4,27 @@
 #include "function.h"
 
 int binarySearchForZero(Function<int, int> * f, int low, int high) {
-  if (f->invoke(high - 1) < 0) {
+  /*if (f->invoke(high - 1) < 0) {
     return high - 1;
   }
   else if (f->invoke(low) > 0) {
     return low;
-  }
-  else {
-    while (low < high - 1) {
-      int mid = (low + high) / 2;
-      if (f->invoke(mid) == 0) {
-        return mid;
-      }
-      else if (f->invoke(mid) > 0) {
-        high = mid - 1;
-      }
-      else {
-        low = mid + 1;
-      }
-    }
+    }*/
+  if (low + 1 == high) {
     return low;
   }
+  while (low < high - 1) {
+    int mid = (low + high) / 2;
+    int srch_pt = f->invoke(mid);
+    if (srch_pt == 0) {
+      return mid;
+    }
+    else if (srch_pt > 0) {
+      high = mid;
+    }
+    else {
+      low = mid;
+    }
+  }
+  return low;
 }
