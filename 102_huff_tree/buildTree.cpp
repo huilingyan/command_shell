b2a67751518106c2a126a5d2473cd9d7e35a358c
hy165 <hy165@duke.edu>
2019-11-11 16:14:43 -0500

    102

--- 102_huff_tree/buildTree.cpp
+++ 102_huff_tree/buildTree.cpp
@@ -1,5 +1,25 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+
+  //construct the priority queue
+  priority_queue_t pq;
+  for (int i = 0; i < 257; i++) {
+    pq.push(new Node(i, counts[i]));
+  }
+  //construct the huffman tree
+  if (pq.size() == 0) {
+    return NULL;
+  }
+  while (pq.size() > 1) {
+    Node * l = pq.top();
+    pq.pop();
+    Node * r = pq.top();
+    pq.pop();
+    Node * comb = new Node(l, r);
+    pq.push(comb);
+  }
+
+  return pq.top();
+}
