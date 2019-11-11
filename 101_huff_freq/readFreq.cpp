c45159b5fcfdddeebc935accb09fd546a82a7472
hy165 <hy165@duke.edu>
2019-11-11 13:55:44 -0500

    101

--- 101_huff_freq/readFreq.cpp
+++ 101_huff_freq/readFreq.cpp
@@ -1,9 +1,15 @@
-#include <stdio.h>
-#include <cstdlib>
 #include "readFreq.h"
 
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>  // std::cout
+#include <istream>
+#include <string>
+#include <vector>  // std::vector
+
 void printSym(std::ostream & s, unsigned sym) {
-  if (sym > 256 ) {
+  if (sym > 256) {
     s << "INV";
   }
   else if (sym == 256) {
@@ -21,4 +27,52 @@ void printSym(std::ostream & s, unsigned sym) {
 }
 uint64_t * readFrequencies(const char * fname) {
   //WRITE ME!
+  int ch;
+  uint64_t * freqn = new uint64_t[256]();
+  std::ifstream f;
+  f.open(fname);
+  if (!f.is_open()) {
+    std::cerr << "Failed to open the file!\n" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+  while ((ch = f.get()) != EOF) {
+    freqn[ch]++;
+  }
+  freqn[256]++;
+  f.close();
+  return freqn;
 }
+/* void sort_lines(std::istream & file) {
+    std::vector<std::string> lines;
+    std::string str;
+
+    while (getline(file, str)) {
+      lines.push_back(str);
+    }
+    std::sort(lines.begin(), lines.end());
+
+    for (std::vector<std::string>::iterator it = lines.begin(); it != lines.end(); ++it) {
+      std::cout << *it << std::endl;
+    }
+    }
+
+    int main(int argc, char ** argv) {
+    std::ifstream files;
+
+    if (argc == 1) {
+      sort_lines(std::cin);
+    }
+    else {
+      for (int i = 1; i < argc; i++) {
+        files.open(argv[i], std::ifstream::in);
+        //if (files.fail()) {
+        if (!files.is_open()) {
+          std::cerr << "Failed to open the file!\n" << std::endl;
+          exit(EXIT_FAILURE);
+        }
+        sort_lines(files);
+        files.close();
+      }
+    }
+    return EXIT_SUCCESS;
+  */
